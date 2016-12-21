9b7b5634aa460e6df3f883c9dc294f2e7ed96793
Tommy Anderson <tommy2030@gmail.com>
2016-12-21 16:55:06 -0500

    Convert indentation to spaces

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,60 @@
 const webpack = require('webpack');
 
 module.exports = {
-	entry: {
-		obercube: './src/client/index.tsx'
-	},
+    entry: {
+        obercube: './src/client/index.tsx'
+    },
 
-	output: {
-		path: `${__dirname}/dist/`,
-		publicPath: '/dist/',
-		filename: './[name].js'
-	},
+    output: {
+        path: `${__dirname}/dist/`,
+        publicPath: '/dist/',
+        filename: './[name].js'
+    },
 
-	resolve: {
-		extensions: ['', '.js', '.jsx', '.ts', '.tsx']
-	},
+    resolve: {
+        extensions: ['', '.js', '.jsx', '.ts', '.tsx']
+    },
 
-	module: {
-		loaders: [
-			{
-				test: /\.jsx?$/,
-				loader: 'babel',
-				include: `${__dirname}/src`,
-			},
-			{
-				test: /\.tsx?$/,
-				loader: 'awesome-typescript-loader',
-				include: `${__dirname}/src`,
-				query: {
-					tsconfig: './tsconfig.json'
-				}
-			}
-		]
-	},
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel',
+                include: `${__dirname}/src`,
+            },
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader',
+                include: `${__dirname}/src`,
+                query: {
+                    tsconfig: './tsconfig.json'
+                }
+            }
+        ]
+    },
 
-	devServer: {
-		historyApiFallback: true,
-		noInfo: true
-	},
+    devServer: {
+        historyApiFallback: true,
+        noInfo: true
+    },
 
-	devtool: 'eval-source-map'
+    devtool: 'eval-source-map'
 };
 
 if (process.env.NODE_ENV === 'production') {
-	module.exports.devtool = 'source-map';
+    module.exports.devtool = 'source-map';
 
-	module.exports.plugins = (module.exports.plugins || []).concat([
-		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: '"production"'
-			}
-		}),
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
 
-		new webpack.optimize.UglifyJsPlugin({
-			compress: {
-				warnings: false
-			}
-		})
-	]);
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ]);
 }
